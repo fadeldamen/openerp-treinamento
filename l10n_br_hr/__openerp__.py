24f1498a6b32a383ba03716abe0c37fa04fa335f
Fadel Damen Schreiner <fadel.damen@gmail.com>
2014-05-21 19:10:14 -0400

    arquivos novos

--- l10n_br_hr/__openerp__.py
+++ l10n_br_hr/__openerp__.py
@@ -27,7 +27,7 @@
     'website': 'http://proge.com.br',
     'description': '''Long description.''',
     'depends': ['hr'],
-    'data': ['l10n_br_hr_view.xml'], # Views, Dados, Wizards
+    'data': ['l10n_br_hr_view.xml',  'hr_employee_view.xml',], # Views, Dados, Wizards
     'demo': [],
     'installable': True,
 }
--- l10n_br_hr/hr_employee.py
+++ l10n_br_hr/hr_employee.py
@@ -1,4 +1,4 @@
-# -*- encoding: utf-8 -*-
+# -*- coding: utf-8 -*-
 ##############################################################################
 # #
 # Copyright (C) 2014 Proge Informática Ltda (<http://www.proge.com.br>). #
@@ -19,5 +19,32 @@
 # along with this program. If not, see <http://www.gnu.org/licenses/>. #
 # #
 ##############################################################################
-import l10n_br_hr
-import hr_employee
+
+from osv import osv, fields
+
+class hr_employee(osv.osv):
+    _name = 'hr.employee'
+    _inherit = 'hr.employee'
+
+    _columns = {
+        'matricula': fields.char(u'Matrícula', size=11),
+        'pis_pasep': fields.char(u'PIS/PASEP', size=15),
+        'carteira_de_trabalho_numero': fields.integer(u'Número da Carteira de Trabalho', size=8),
+        'carteira_de_trabalho_serie': fields.integer(u'Série da Carteira de Trabalho', size=5),
+        'deficiencia': fields.selection(
+         (
+             ('0', u'Não possui'),
+             ('1', u'Física'),
+             ('2', u'Auditiva'),
+             ('3', u'Visual'),
+             ('4', u'Intelectual (Mental)'),
+             ('5', u'Múltipla'),
+             ('6', u'Rabilitado'),
+         ), u'Deficiência'
+        ),
+        'etnia_id': fields.many2one('l10n_br_hr.etnia', u'Etnia'),
+
+    }
+
+hr_employee()
+
